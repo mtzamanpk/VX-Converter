69a4a3d6bdfe95d6af8df0ba3047cda1c4ac33dd
Muhammad Zaman <98908904+mtzamanpk@users.noreply.github.com>
2024-01-11 16:26:33 -0600

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,21 +2,26 @@ import re
 import os
 import discord
 from discord.ext import commands
-from keep_alive import keep_alive
 from flask import Flask
 import threading
+from keep_alive import keep_alive
+
+keep_alive()
 
 TOKEN = os.getenv('DISCORD_TOKEN')
+BOT_PREFIX = '!'
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix='', intents=intents)
+bot = commands.Bot(command_prefix=BOT_PREFIX, intents=intents)
+
 
 @bot.event
 async def on_ready():
   print(f'Logged in as {bot.user.name}')
   print('------')
+
 
 @bot.event
 async def on_message(message):
@@ -27,15 +32,28 @@ async def on_message(message):
   if converted_message != message.content:
     sent_message = await message.channel.send(
         f'{message.author.mention} Converted link: {converted_message}')
+
     # Delete the original message sent by the author
     await delete_unconverted_message(message, sent_message)
+
   await bot.process_commands(message)
+
 
 def convert_links(content):
   # Convert TikTok links
   tiktok_pattern = r'https://vm.tiktok.com/'
   tiktok_replacement = r'https://vm.vxtiktok.com/'
   content = re.sub(tiktok_pattern, tiktok_replacement, content)
+
+  # Convert Instagram links
+  instagram_pattern = r'https://instagram.com/'
+  instagram_replacement = r'https://ddinstagram.com/'
+  content = re.sub(instagram_pattern, instagram_replacement, content)
+
+  # Convert web TikTok links
+  web_tiktok_pattern = r'https://www.tiktok.com/'
+  web_tiktok_replacement = r'https://vxtiktok.com/'
+  content = re.sub(web_tiktok_pattern, web_tiktok_replacement, content)
 
   # Convert "x" links
   x_pattern = r'https://x.com/'
@@ -47,17 +65,8 @@ def convert_links(content):
   twitter_replacement = r'https://vxtwitter.com/'
   content = re.sub(twitter_pattern, twitter_replacement, content)
 
-  # Convert Instagram links
-  instagram_pattern = r'https://www.instagram.com/'
-  instagram_replacement = r'https://www.ddinstagram.com/'
-  content = re.sub(instagram_pattern, instagram_replacement, content)
-
-  # Convert web TikTok links
-  web_tiktok_pattern = r'https://www.tiktok.com/'
-  web_tiktok_replacement = r'https://vxtiktok.com/'
-  content = re.sub(web_tiktok_pattern, web_tiktok_replacement, content)
-
   return content
+
 
 async def delete_unconverted_message(original_message, converted_message):
   try:
@@ -74,17 +83,3 @@ async def delete_unconverted_message(original_message, converted_message):
     print('Message not found for deletion.')
   except discord.HTTPException as e:
     print(f'An error occurred while deleting a message: {e}')
-
-bot.run(TOKEN)
-app = Flask(__name__)
-
-
-@app.route('/')
-def keep_alive():
-  return "Bot is running!"
-
-def run():
-  app.run(host='0.0.0.0', port=8080)
-
-if __name__ == '__main__':
-  threading.Thread(target=run).start()
